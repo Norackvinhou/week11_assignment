1554536c349a557adba07b7eae67e4c9411837cb
Norackvinhou <74971465+Norackvinhou@users.noreply.github.com>
2021-06-13 20:26:35 +0700

    intergrate admin page

---
+++ resources/views/layout/auth.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    
+    @include('layout.auth.meta')
+    <title>SB Admin 2 - Login</title>
+
+    <!-- Custom fonts for this template-->
+    @include('layout.auth.style')
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        @yield('content')
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    @include('layout.auth.script')
+</body>
+
+</html>
